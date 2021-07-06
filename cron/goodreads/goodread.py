0d2280e1e831e882ed854332c6e4caf8dadc48eb
deltamaze <wcpooley@gmail.com>
2021-07-05 20:03:47 -0500

    refactor a little

--- cron/goodreads/goodread.py
+++ cron/goodreads/goodread.py
@@ -5,53 +5,24 @@ from time import sleep
 from random import randint
 from datetime import date
 
-maxPageNumber = 100
-
-
-class bookModel:
-    def __init__(self, bookurl, name, author, ratingText, timestamp, listName):
-        self.bookurl = bookurl
-        self.name = name
-        self.author = author
-        self.ratingText = ratingText
-        self.timestamp = timestamp
-        self.listName = listName
-
+import goodreadModules as grm
 
 try:
     # target lists / urls
-    listUrls = ["https://www.goodreads.com/list/show/3077",
-                "https://www.goodreads.com/list/show/7",
-                "https://www.goodreads.com/list/show/6",
-                "https://www.goodreads.com/list/show/16",
-                "https://www.goodreads.com/list/show/30",
-                "https://www.goodreads.com/list/show/53",
-                "https://www.goodreads.com/list/show/52",
-                "https://www.goodreads.com/list/show/74",
-                "https://www.goodreads.com/list/show/73",
-                "https://www.goodreads.com/list/show/1384",
-                "https://www.goodreads.com/list/show/71",
-                "https://www.goodreads.com/list/show/1414",
-                "https://www.goodreads.com/list/show/14",
-                "https://www.goodreads.com/list/show/8163",
-                "https://www.goodreads.com/list/show/4088",
-                "https://www.goodreads.com/list/show/71525",
-                "https://www.goodreads.com/list/show/3079",
-                "https://www.goodreads.com/list/show/3078",
-                "https://www.goodreads.com/list/show/3077"]
+    listUrls = grm.getTargetLists()
     # get loop through each url
     webRequestMadeThisExecution = 0
+    maxPageNumber = 100
+    maxNumberOfWebRequests = 100
     targetListIndex = 0
-    targetPage = 1
-    conn = sqlite3.connect('sqlite.db')
+    conn = sqlite3.connect('goodreads.db')
     # get last polled list item and page number
-    while webRequestMadeThisExecution < 100:
+    while webRequestMadeThisExecution < maxNumberOfWebRequests:
+        targetPage = 1
         targetListIndex += 1
         areThereMorePagesToPullForThisList = True
         listName = ""
-        while (areThereMorePagesToPullForThisList
-                and targetPage <= maxPageNumber
-                and webRequestMadeThisExecution <= 100):
+        while (areThereMorePagesToPullForThisList):
             # load url
 
             parsedBooks = []
@@ -68,15 +39,18 @@ try:
                 author = book.find('a', attrs={'class': 'authorName'}).text
                 ratingText = book.find(
                     'span', attrs={'class': 'minirating'}).text
-                thisBook = bookModel(bookurl, name, author, ratingText,
-                                     date.today(), listName)
+                thisBook = grm.bookModel(bookurl, name, author, ratingText,
+                                         date.today(), listName)
                 parsedBooks.append(thisBook)
             print(f"Finished Page {str(targetPage)} for List {listName}")
 
-            if len(booksOnPage) != 100:
+            targetPage += 1
+            webRequestMadeThisExecution += 1
+            if (len(booksOnPage) != 100
+                    or targetPage <= maxPageNumber
+                    or webRequestMadeThisExecution <= maxNumberOfWebRequests):
                 areThereMorePagesToPullForThisList = False
             sleep(randint(2, 10))
-            targetPage += 1
             # save results to sqllite
             # update sqllite table to indicate list id and page number
 
---
+++ cron/goodreads/goodreadModules.py
@@ -0,0 +1,30 @@
+class bookModel:
+    def __init__(self, bookurl, name, author, ratingText, timestamp, listName):
+        self.bookurl = bookurl
+        self.name = name
+        self.author = author
+        self.ratingText = ratingText
+        self.timestamp = timestamp
+        self.listName = listName
+
+
+def getTargetLists():
+    return ["https://www.goodreads.com/list/show/3077",
+            "https://www.goodreads.com/list/show/7",
+            "https://www.goodreads.com/list/show/6",
+            "https://www.goodreads.com/list/show/16",
+            "https://www.goodreads.com/list/show/30",
+            "https://www.goodreads.com/list/show/53",
+            "https://www.goodreads.com/list/show/52",
+            "https://www.goodreads.com/list/show/74",
+            "https://www.goodreads.com/list/show/73",
+            "https://www.goodreads.com/list/show/1384",
+            "https://www.goodreads.com/list/show/71",
+            "https://www.goodreads.com/list/show/1414",
+            "https://www.goodreads.com/list/show/14",
+            "https://www.goodreads.com/list/show/8163",
+            "https://www.goodreads.com/list/show/4088",
+            "https://www.goodreads.com/list/show/71525",
+            "https://www.goodreads.com/list/show/3079",
+            "https://www.goodreads.com/list/show/3078",
+            "https://www.goodreads.com/list/show/3077"]
