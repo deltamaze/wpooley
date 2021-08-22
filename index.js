9e9a1491ba4b0d09b9db2db4b2086a38a97945f4
deltamaze <wcpooley@gmail.com>
2021-08-22 11:43:47 -0500

    setting up navbar

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // lint here: https://validatejavascript.com/
 // responsive example: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_topnav
+function mobileToggleNavbar() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbarClass") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbarClass";
+    }
+}
